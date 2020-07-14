7c4673261cc163c5ec15df9ec1446aba860466b1
sanujkul <sanujkul@gmail.com>
2020-07-14 07:53:26 -0700

    Added one more LED and created two MAIN tasks

--- RTOS-01B-Kernel-Internels-STM/main.c
+++ RTOS-01B-Kernel-Internels-STM/main.c
@@ -19,11 +19,13 @@ Note:
 #define GPIOA_MODE_REG (GPIOA_BASE_ADDR + GPIO_MODE_REG_OFFSET)
 #define MODE_OUT 1
 #define P5_MODE_OUT (1U<<10)
+#define P6_MODE_OUT (1U<<12)
 
 #define GPIO_OUTPUT_DATA_REG_OFFSET 0x14UL
 #define GPIOA_OUTPUT_DATA_REG (GPIOA_BASE_ADDR + GPIO_OUTPUT_DATA_REG_OFFSET)
 
-#define GREEN_LED (1UL<<5) //As it is the 5th pin and will be used to toggle LED
+#define GREEN_USR_LED (1UL<<5) //As it is the 5th pin and will be used to toggle LED
+#define GREEN_EXT_LED (1UL<<6) //Connected to PA6
 
 #define RCC_BASE_ADDR 0x40023800UL
 #define AHB1ENR_OFFSET 0x30UL
@@ -32,41 +34,88 @@ Note:
 
 //GLOBAL VARIABLES
 volatile uint32_t tick;	//Ticks of clock
-volatile uint32_t _tick; //
+volatile uint32_t _tick; //A variable that is used for work on tick
 
 // FUNCTION DEFINITIONS
 void GPIO_Init(void);
-
-
-
-
+uint32_t getTick(void);
+void Delays(uint32_t seconds);
+void greenUsrOn(void);
+void greenUsrOff(void);
+void greenExtOn(void);
+void greenExtOff(void);
+int greenUsrMain(void);
+int greenExtMain(void);
+	
 int main(){
-
+	uint32_t volatile start = 0U;
 	//1,2. Initializing the GPIO pins
 	GPIO_Init();
 	
-	//3. BLINKING
-	volatile uint32_t *gpioAOutDataR = (uint32_t*) GPIOA_OUTPUT_DATA_REG;
-	volatile int i = 0;
+	if(start){
+		greenUsrMain();
+	}else{
+		greenExtMain();
+	}
+	
 	while(1){
-		//3.a. ON	
-		//GPIOA->ODR |= GREEN_LED;		//Either this way or
-		*gpioAOutDataR |= GREEN_LED;
-		
-		//Delay
-		for(i=0; i<1000000000; i++);
-	
-		//3.b. OFF
-		*gpioAOutDataR &= ~GREEN_LED;
-	
-		//Delay
-		for(i=0; i<1000000000; i++);
-
+		//INFINITTE LOOP
 	}
 	
 }
 
+///////////////////////////////////////////////////////////////////////////////////////
+/// TWO MAIN FUNCTIONS THAT WILL BE LIKE TASKS
+///////////////////////////////////////////////////////////////////////////////////////
+int greenUsrMain(void){
+	while(1){
+		greenUsrOn();
+		Delays(50);
+		greenUsrOff();
+		Delays(50);
+	}
+}
 
+int greenExtMain(void){
+	while(1){
+		greenExtOn();
+		Delays(50);
+		greenExtOff();
+		Delays(50);
+	}
+}
+
+
+//Thouhg this compiler shows that there is no previous definition of SysTick_Handler, 
+//I verified in debug mode that address 0x0000003C where SysTick address has to be saved (Refer vector table)
+//has the address for this function!! So its working!
+//A weak function defined in startup file
+void SysTick_Handler(void){
+	++tick;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Functions made for Delay
+///////////////////////////////////////////////////////////////////////////////////////
+uint32_t getTick(void){
+	__disable_irq();
+	_tick = tick;
+	__enable_irq();
+	
+	return _tick;
+}
+
+void Delays(uint32_t milliSeconds){
+	milliSeconds *= 16;						//Converting to number of ticks needed to achieve this delay
+	uint32_t temp = getTick();
+	while((getTick() - temp < milliSeconds)){}
+}
+
+
+///////////////////////////////////////////////////////////////
+//The LEDs ON OFF FUNCTIONS, and initializing function
+///////////////////////////////////////////////////////////////
+//Initializing the GPIOS
 void GPIO_Init(void){
 	//1. Enabling the CLOCK FOR GPIOA
 	//RCC->AHB1ENR |= GPIOA_CLK_ENABLE; //Either this way or - 
@@ -76,19 +125,32 @@ void GPIO_Init(void){
 	//2. SETTING GPIOA Pin 5 (GREEN LED) as OUTPUT 	
 	//GPIOA->MODER |= P5_MODE_OUT;  //Either this way or
 	uint32_t *gpioAModeR = (uint32_t*) GPIOA_MODE_REG;
-	*gpioAModeR |= P5_MODE_OUT;
+	*gpioAModeR |= (P5_MODE_OUT | P6_MODE_OUT);
 
 	//3. Configuring the System Clock
 	SysTick_Config(SystemCoreClock/1000); //To make it in milli second
 	__enable_irq();
 
 }
-
-//Thouhg this compiler shows that there is no previous definition of SysTick_Handler, 
-//I verified in debug mode that address 0x0000003C where SysTick address has to be saved (Refer vector table)
-//has the address for this function!! So its working!
-//A weak function defined in startup file
-void SysTick_Handler(void){
-	++tick;
+void greenUsrOn(void){
+	//3. BLINKING
+	//volatile uint32_t *gpioAOutDataR = (uint32_t*) GPIOA_OUTPUT_DATA_REG;
+	//*gpioAOutDataR ^= (GREEN_USR_LED); //Either this way or
+	GPIOA->ODR |= GREEN_USR_LED;		
 }
+void greenUsrOff(void){
+	//3. BLINKING
+	//volatile uint32_t *gpioAOutDataR = (uint32_t*) GPIOA_OUTPUT_DATA_REG;
+	//*gpioAOutDataR ^= (GREEN_USR_LED); //Either this way or
+	GPIOA->ODR &= ~GREEN_USR_LED;		
+}
+
+void greenExtOn(void){
+	GPIOA->ODR |= GREEN_EXT_LED;		
+}
+void greenExtOff(void){
+	GPIOA->ODR &= ~GREEN_EXT_LED;		
+}
+
+
 
